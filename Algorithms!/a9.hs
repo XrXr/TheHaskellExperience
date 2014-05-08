4eb27f8b1bee8a6186bd69d977d7e78cad0e992f
XrXr <xrsquared@gmail.com>
2014-05-08 15:02:06 -0400

    Three questions

--- Algorithms!/a9.hs
+++ Algorithms!/a9.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 fib0 :: Int -> Int
 fib0 n
     | n <= 0    = error "n must be larger than 0"
@@ -5,4 +7,19 @@ fib0 n
 
 fib3 :: Int -> Integer
 fib3 n = squence !! n
-     where squence = 1:1:1:zipWith3 (\a b c -> a+b+c) squence (tail squence) (tail $ tail squence)
+    where squence = 1:1:1:zipWith3 (\a b c -> a+b+c) squence (tail squence) (tail $ tail squence)
+
+largest_two :: (Num a, Ord a) => [a] -> a
+largest_two l = (last sorted) + (last . init $ sorted)
+    where sorted = sort l
+
+smallest_half :: (Num a, Ord a) => [a] -> a
+smallest_half l = sum $ take (length sorted `div` 2) sorted
+    where sorted = sort l
+
+median :: (Num a, Ord a, Fractional a) => [a] -> a
+median l
+    | length sorted `mod` 2 == 0 = (sorted !! middle + sorted !! (middle - 1)) / 2
+    | otherwise                  = sorted !! middle
+    where sorted = sort l
+          middle = length sorted `div` 2
