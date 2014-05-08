d86391b9b4115b3a63c3b749969041bdb3b25f14
XrXr <xrsquared@gmail.com>
2014-05-08 00:09:15 -0400

    Fix to fib0

--- Algorithms!/a9.hs
+++ Algorithms!/a9.hs
@@ -1,7 +1,8 @@
 fib0 :: Int -> Int
-fib0 <=0 = error "a must be larger than 0"
-fib0 n = n ^ 2
+fib0 n
+    | n <= 0    = error "n must be larger than 0"
+    | otherwise = n ^ 2
 
 fib3 :: Int -> Integer
 fib3 n = squence !! n
-     where squence = 1:1:1:zipWith3 (\a b c -> a+b+c) squence (tail squence) (tail (tail squence))
+     where squence = 1:1:1:zipWith3 (\a b c -> a+b+c) squence (tail squence) (tail $ tail squence)
