7436e25f6a28815d3dc666ac9838adff779a0b16
XrXr <xrsquared@gmail.com>
2014-05-09 15:22:39 -0400

    One problem

--- Algorithms!/a9.hs
+++ Algorithms!/a9.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.Maybe
 
 fib0 :: Int -> Int
 fib0 n
@@ -19,7 +20,35 @@ smallest_half l = sum $ take (length sorted `div` 2) sorted
 
 median :: (Num a, Ord a, Fractional a) => [a] -> a
 median l
-    | length sorted `mod` 2 == 0 = (sorted !! middle + sorted !! (middle - 1)) / 2
-    | otherwise                  = sorted !! middle
+    | isEven     = (sorted !! middle + sorted !! (middle - 1)) / 2
+    | otherwise  = sorted !! middle
     where sorted = sort l
           middle = length sorted `div` 2
+          isEven = length sorted `mod` 2 == 0 
+
+majority :: (Num a, Ord a) => [a] -> Maybe a
+-- In an even length sorted list where a majority exist, its either
+-- [a,a,a,a,b,b] or [b,b,a,a,a,a] both has the majority element at 
+-- length `div` 2
+-- in an odd length list
+-- [a,a,a,b,b] or [b,b,a,a,a] this still hold
+majority l
+    | null l     = Nothing
+    | isEven       = if same (take (middle + 1) sorted)
+                   || same (drop (middle - 1) sorted)
+                   then Just $ sorted !! middle else Nothing
+    | otherwise  = if same (take (middle + 1) sorted)
+                   || same (drop middle sorted)
+                   then Just $ sorted !! middle else Nothing
+    where sorted = sort l
+          isEven = length sorted `mod` 2 == 0 
+          middle = length sorted `div` 2
+          same l@(x:xs) = replicate (length l) x == l
+
+majority' :: Eq a => [a] -> Maybe a
+-- Polymorphic version
+majority' [] = Nothing
+majority' l  = foldl (\acc x -> if isMajority (find x) then Just x else acc) Nothing l 
+    where middle       = length l `div` 2
+          find e       = elemIndices e l
+          isMajority a = length a >= middle + 1
